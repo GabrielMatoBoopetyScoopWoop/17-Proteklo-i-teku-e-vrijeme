d6e73116f16645b8d382380f0a033cf3e6d22f4f
GabrielMato <58472077+GabrielMatoBoopetyScoopWoop@users.noreply.github.com>
2021-10-04 17:34:45 +0200

    Add files via upload

---
+++ ProtekloITekuceVrijemeGabrielMatosevic04102021/ProtekloITekuceVrijemeGabrielMatosevic04102021/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProtekloITekuceVrijemeGabrielMatosevic04102021
+{
+    public partial class Form1 : Form
+    {
+        DateTime pocetno = DateTime.Now;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "Trenutno: " + DateTime.Now.ToLongTimeString();
+            TimeSpan proteklo = new TimeSpan();
+            proteklo = proteklo = DateTime.Now - pocetno;
+            toolStripStatusLabel2.Text = "Proteklo: " +
+            proteklo.Hours.ToString() + ":" + proteklo.Minutes.ToString() + ":" + proteklo.Seconds.ToString();
+        }
+    }
+}
